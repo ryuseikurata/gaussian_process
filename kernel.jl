75940a37d457317de1b093fe79efde8af189f9c7
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-06-14 18:37:41 +0900

    feat: add kernel

---
+++ kernel.jl
@@ -0,0 +1,33 @@
+abstract type Kernel end
+
+function cov(k::Kernel, xs1, xs2)
+    # covariance matrix
+
+    # xs1の1軸の寸法
+    n1 = size(xs1, 1)
+
+    # xs2の1軸の寸法
+    n2 = size(xs2, 1)
+
+    # 全要素0の配列
+    c = zeros(n1, n2)
+
+    # 1から寸法ぶん
+    for i in 1:n1
+        for j in i:n2
+            c[i,j] = ker(k, xs1[i, :], xs2[j,:])
+        end
+    end
+end
+
+"""
+Gaussian Kernel
+"""
+
+mutable struct GasssianKernel <: Kernel
+    theta::Float64
+end
+
+function ker(k::GaussianKernel, x1, x2)
+    exp(- sum((x1 - x2).^2) / k.theta)
+end
